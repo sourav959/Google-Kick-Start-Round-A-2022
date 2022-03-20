57be0f8647361de7d563b15e4a74e83274fdf804
Sourav Gupta <46631652+sourav959@users.noreply.github.com>
2022-03-20 11:53:08 +0530

    Update Solution-1-Speed-Typing.java

--- Solution-1-Speed-Typing.java
+++ Solution-1-Speed-Typing.java
@@ -1,1 +1,26 @@
-
+import java.util.*;
+class Solution{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        for(int test=0;test<x;test++){
+            String str1=sc.next();
+            String str2=sc.next();
+            int m=str1.length(),n=str2.length();
+            int i=0,j=0,ans=0;
+            while(j<n && i<m){
+                if(str1.charAt(i)==str2.charAt(j))
+                    i++;
+                else
+                    ans++;
+                j++;
+            }
+            if(i==m){
+                System.out.println("Case #"+(test+1)+": "+(ans+n-j));
+            }else{
+                System.out.println("Case #"+(test+1)+
+                ": IMPOSSIBLE");
+            }
+        }
+    }
+}
